ea3e758b39c7291125a01fbe3b095b9a25f59004
Azuki <theorybreakerhacks@gmail.com>
2022-12-25 20:06:22 +0100

    - Remove precompiled header from OpenDisplay - Rename tDisplay to Display (naming convention) - Re-do virtual function table of Display

---
+++ OpenDisplay/Display/Display.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <Windows.h>
+
+class Display
+{
+public:
+    DWORD* CreateInstance(DWORD* instance);
+
+    virtual void sub_10026AF0();
+    virtual void sub_10026B20();
+    virtual void sub_10026600();
+    virtual void sub_1001FF00();
+    virtual void sub_10026B00();
+    virtual void sub_1000A180();
+    virtual void sub_100042C0();
+    virtual void sub_10026B10();
+    virtual void nullsub_2();
+    virtual void sub_10007970();
+    virtual void nullsub_4();
+    virtual void sub_10025D30();
+    virtual void sub_10025F80();
+    virtual void sub_10026070();
+    virtual void sub_100262E0();
+    virtual void sub_100260E0();
+    virtual void sub_10026210();
+    virtual void sub_10026350();
+    virtual void sub_100263F0();
+    virtual void sub_10026550();
+    virtual void sub_10026490();
+    virtual void sub_10025BF0();
+    virtual void sub_10026560();
+    virtual void sub_10026570();
+    virtual void sub_10026610();
+    virtual void sub_100251A0();
+    virtual void sub_10025CC0();
+    virtual void sub_100265A0();
+};
--- OpenDisplay/dllmain.cpp
+++ OpenDisplay/dllmain.cpp
@@ -1,5 +1,5 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
+#include <Windows.h>
 
 static HMODULE dword_1004E318;
 

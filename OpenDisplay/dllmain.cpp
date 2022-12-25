70e8b2c3bae13ca6275f779531dd2b06ec16c1dc
Azuki <theorybreakerhacks@gmail.com>
2022-12-25 18:44:01 +0100

    Add project files.

---
+++ OpenDisplay/dllmain.cpp
@@ -0,0 +1,16 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+static HMODULE dword_1004E318;
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+        dword_1004E318 = hModule;
+
+    return TRUE;
+}
+
---
+++ OpenDisplay/tDisplay.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <Windows.h>
+
+class tDisplay
+{
+public:
+    DWORD* CreateInstance(DWORD* instance);
+
+    virtual void vf0();
+    virtual void vf4();
+    virtual void vf8();
+    virtual void vfC();
+    virtual void vf10();
+    virtual void vf14();
+    virtual void vf18();
+    virtual void vf1C();
+    virtual void vf20();
+    virtual void vf24();
+    virtual void vf28();
+    virtual void vf2C();
+    virtual void vf30();
+    virtual void vf34();
+    virtual void vf38();
+    virtual void vf3C();
+    virtual void vf40();
+    virtual void vf44();
+    virtual void vf48();
+    virtual void vf4C();
+    virtual void vf50();
+    virtual void vf54();
+    virtual void vf58();
+    virtual void vf5C();
+    virtual void vf60();
+    virtual void vf64();
+    virtual void vf68();
+    virtual void vf6C();
+    virtual void vf70();
+    virtual void vf74();
+    virtual void vf78();
+    virtual void vf7C();
+    virtual void vf80();
+    virtual void vf84();
+    virtual void vf88();
+    virtual void vf8C();
+};
